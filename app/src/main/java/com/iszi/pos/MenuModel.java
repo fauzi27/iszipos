c664f49e562113c6366c0d3178744c5cbcd3f9b2
fauzi27 <44411728+fauzi27@users.noreply.github.com>
2026-03-23 13:06:27 +0700

    update app/src/main/java/com/iszi/pos/MenuModel.java

--- app/src/main/java/com/iszi/pos/MenuModel.java
+++ app/src/main/java/com/iszi/pos/MenuModel.java
@@ -4,7 +4,6 @@ public class MenuModel {
     private String id, name, category, image, icon;
     private int price, stock, capitalPrice;
 
-    // Konstruktor kosong wajib untuk Firebase
     public MenuModel() {}
 
     public MenuModel(String id, String name, String category, int price, int stock, int capitalPrice) {
@@ -19,10 +18,17 @@ public class MenuModel {
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
     public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
     public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
     public int getPrice() { return price; }
+    public void setPrice(int price) { this.price = price; }
     public int getStock() { return stock; }
+    public void setStock(int stock) { this.stock = stock; } // Ini yang bikin Error!
     public int getCapitalPrice() { return capitalPrice; }
+    public void setCapitalPrice(int capitalPrice) { this.capitalPrice = capitalPrice; }
     public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
     public String getIcon() { return icon; }
+    public void setIcon(String icon) { this.icon = icon; }
 }
